2d6ec50b1f30b0372108723d08e0651b6a87748f
hani <hani@DESKTOP-5I66P1U.localdomain>
2024-08-05 06:46:00 +0300

    naimaa

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [item, setItem] = useState("");
+
+  const toggleCartItem = () => {
+    setItem(item ? "" : "in-cart");
+  };
+
   return (
-    <li className="">
+    <li className={item}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={toggleCartItem}>
+        {item ? "Remove from Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
